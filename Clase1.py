5b3ca3644713c400dc6d61d1c007b94e0f8b9fc6
christian garcia <christian.garciap@hotmail.com>
2023-11-28 15:01:10 -0500

    mi primer commit

---
+++ Clase1.py
@@ -0,0 +1,11 @@
+a="hola, bienvenidos a mi primer codigo"
+b='estoy muy "emocionado"'
+
+print(a+b)
+print(a+" "+b)
+print(a,b)
+a=3
+b=5
+c=4
+
+print(f"el primer numero impar es {a}, el segundo es {b}, pero {c} es par")
