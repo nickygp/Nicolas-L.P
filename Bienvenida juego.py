0db1eb939cf160f8766550f0bfd984b6b7dac043
nickygp <152317624+nickygp@users.noreply.github.com>
2023-11-29 16:51:44 -0500

    Add files via upload

---
+++ Bienvenida juego.py
@@ -0,0 +1,8 @@
+a=("!Hola! Bienvenido a uno de los juegos más populares,")
+b=("bienvenido a 'piedra, papel o tijera'")
+print(a,b)
+b=1
+c=2
+print(f"¿Listo para divertirte? Presiona {b} si deseas jugar o {c} si deseas salir.")
+e=("Creado por: Nicolás García")
+print(e)
